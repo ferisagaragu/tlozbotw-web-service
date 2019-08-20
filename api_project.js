5c958856eb2a3b94240c4151959ce5601e2b9215
Fernando García <ferisagaragu@gmail.com>
2019-08-20 16:37:23 -0500

    Updates

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "TLOZBOTW - Service",
+  "version": "0.0.1",
+  "description": "build",
+  "title": "tlozbotw - restApi",
+  "url": "https://localhost:3400",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-08-20T21:36:26.601Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});

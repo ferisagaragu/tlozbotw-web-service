60aa76c6c441d5034992716905fe6ad3349c8413
Fernando García <ferisagaragu@gmail.com>
2019-08-21 22:21:46 -0500

    add firebase and list users

--- src/services/restful.ts
+++ src/services/restful.ts
@@ -1,10 +1,17 @@
 import LoginRest from "./login.rest";
+import BowRest from "./bow.rest";
 
 const loginRest = new LoginRest();
+const bowRest = new BowRest();
 
 export function init() {
   //LOGIN
   loginRest.devToken();
+
   loginRest.login();
   loginRest.registerUser();
+  loginRest.getUsers();
+
+  //BOW
+  bowRest.getBows();
 }

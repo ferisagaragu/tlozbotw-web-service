9cc338b66b7ce926de59832b040ef30c4ae1364f
Fernando García <ferisagaragu@gmail.com>
2019-08-24 00:00:32 -0500

    documentation petition

--- src/services/notification.res.ts
+++ src/services/notification.res.ts
@@ -8,50 +8,29 @@ class NotificationRest {
     @api {delete} /deleteNotification deleteNotification
     @apiVersion 0.0.1
     @apiGroup Notification
-    @apiParamExample {json} Request-Example:
-      {
-        "email": "your user",
-        "password": "your password"
-      }
-    @apiParam {String} email Correo de prueba: ferisagaragu@gmail.com
-    @apiParam {String} password Contraseña de prueba: 1234//
-    @apiDescription Servicio para iniciar sesión
+    @apiParam {String} id Id del usuario al que pertenece la notificación
+    @apiParam {Number} notifyId Id de la notificación a eleminar
+    @apiDescription Servicio para eliminar la notificacion de un usuario
     @apiSuccessExample {json} HTTP/1.1 200 OK
       {
-        "token": "your api token",
-        "userData": {
-          "uid": "your id",
-          "email": "your email",
-          "name": "your name",
-          "photo": "your url photo",
-          "role": your number role
-        }
+        "message": "La notificacion fue eliminada de manera satisfactoria"
       }
     @apiErrorExample {json} HTTP/1.1 400 Bad Request
       {
         "code": "Faltan parametros para hacer la petición",
-        "message": "Hubo un problema al iniciar sesión"
+        "message": "Hubo un problema al eliminar la notificación"
       }
-    @apiErrorExample {json} HTTP/1.1 403 Forbidden - case 1
+    @apiErrorExample {json} HTTP/1.1 403 Forbidden
       {
-        "code": {
-          ...error data
-        },
-        "message": "El usuario o contraseña son incorrectos"
-      }
-    @apiErrorExample {json} HTTP/1.1 403 Forbidden - case 2
-      {
-        "code": {
-          ...error data
-        },
-        "message": "El usuario ingresado no esta registrado"
+        "code": "El id de usuario o de la notificacion no se encontro",
+        "message": "Hubo un problema al eliminar la notificación"
       }
     @apiErrorExample {json} HTTP/1.1 500 Internal Server Error
       {
         "code": {
           ...error data
         },
-        "message": "Hubo un problema al iniciar sesión"
+        "message": "Hubo un problema al eliminar la notificación"
       }
   */
   public deleteNotification(): void {
@@ -63,18 +42,31 @@ class NotificationRest {
         firebase.database().ref(`notify/${id}/${notifyId}`).once('value',
           (data: any) => {
             const updateData = data.val();
-            updateData.see = true;
-
-            firebase.database().ref(`notify/${id}/${notifyId}`)
-              .update(updateData, () => {
-                response._200(
-                  resp, 
-                  null, 
-                  0, 
-                  'La notificacion fue eliminada de manera satisfactoria'
-                );
-              }
-            );
+            
+            if (updateData) {
+              updateData.see = true;
+              firebase.database().ref(`notify/${id}/${notifyId}`)
+                .update(updateData, (error: any) => {
+                  if (error) {
+                    console.log('error');
+                    throw error;
+                  }
+                  
+                  response._200(
+                    resp, 
+                    null, 
+                    0, 
+                    'La notificacion fue eliminada de manera satisfactoria'
+                  );
+                }
+              );
+            } else {
+              response._403(
+                resp, 
+                'El id de usuario o de la notificacion no se encontro',
+                'Hubo un problema al eliminar la notificación'
+              );
+            }
           }
         );
       } else {

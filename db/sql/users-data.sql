181262456cf9d612794df9ebfbe8f94d5467dfe1
Fernando García <ferisagaragu@gmail.com>
2019-08-26 13:49:48 -0500

    Config and service

--- db/sql/users-data.sql
+++ db/sql/users-data.sql
@@ -2,6 +2,10 @@
 -- Query: SELECT * FROM tlozbotw.users
 LIMIT 0, 2000
 
--- Date: 2019-08-07 23:16
+-- Date: 2019-08-25 22:27
 */
-INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('FNfi3a5O8sNOI7B3v1HOoCBckOE2','ferisagaragu@gmail.com','Fernando Fernny','https://lh4.googleusercontent.com/--kQk-D8sBIs/AAAAAAAAAAI/AAAAAAAAGsQ/E_51G-s1xTQ/photo.jpg',0);
+INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('B5UaPBKkOHYHTznm3MAKL51aZpl1','javabrain.email@gmail.com','Test prueba 2','https://fotografias-neox.atresmedia.com/clipping/cmsimages02/2017/12/31/6263DDC6-6E50-479E-A51D-6EA8F206B5D3/63.jpg',0);
+INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('qd9HwvPIO2a0XtxV1Rl9aeBKAqh2','ferisagaragu@gmail.com','Fernando Isaías García','https://datainfox.com/wp-content/uploads/2019/06/kratos-t.jpg',1);
+INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('VqMnDam1fQgru4bZEYT3yGQ2yfJ3','fernnypay@yahoo.com','fernnypay@yahoo.com','https://scontent.cdninstagram.com/vp/768d366914c0a79eadae628a8b67bb52/5DC163AF/t51.2885-15/e35/c0.134.1080.1080/s480x480/66326556_2390444491197591_3495239699137757033_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com',0);
+INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('zmqixDaO5xQS7lfq4tZUfRFUHDY2','medina.enid@gmail.com','Enid Medina','https://scontent.cdninstagram.com/vp/768d366914c0a79eadae628a8b67bb52/5DC163AF/t51.2885-15/e35/c0.134.1080.1080/s480x480/66326556_2390444491197591_3495239699137757033_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com',1);
+INSERT INTO `users` (`id`,`email`,`name`,`photo`,`role`) VALUES ('ZmreeK6cjUWzOLl3sIKAJ2TSGI43','ferisagaragu@hotmail.com','Test prueba','https://scontent.cdninstagram.com/vp/768d366914c0a79eadae628a8b67bb52/5DC163AF/t51.2885-15/e35/c0.134.1080.1080/s480x480/66326556_2390444491197591_3495239699137757033_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com',0);
